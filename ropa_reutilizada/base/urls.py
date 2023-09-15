0edc366a94a8f262a7ed4bd3827448c47592a76a
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-09-15 06:50:28 -0600

    quitar tablas

--- ropa_reutilizada/base/urls.py
+++ ropa_reutilizada/base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('lista_cuentas/', views.lista_cuentas, name='lista_cuentas'),
+    
 ]
--- ropa_reutilizada/base/views.py
+++ ropa_reutilizada/base/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Cuenta
+
 
 # clase (herencia)
-def lista_cuentas(request):
-    cuentas = Cuenta.objects.all()
-    return render(request, 'lista_cuentas.html', {'cuentas': cuentas})
