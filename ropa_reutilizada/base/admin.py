59fad4e2fe7b9bf2ca224c010f93b667f914c9cc
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-09-15 14:15:54 -0600

    Producto listos

--- ropa_reutilizada/base/admin.py
+++ ropa_reutilizada/base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import IProducto, ImagenProducto
 
+class ImagenProductoInline(admin.TabularInline):  # Opciones: admin.StackedInline o admin.TabularInline
+    model = ImagenProducto
+    extra = 1  # Puedes ajustar este valor según la cantidad deseada de imágenes para un producto
 
+class IProductoAdmin(admin.ModelAdmin):
+    inlines = [ImagenProductoInline]
+
+admin.site.register(IProducto, IProductoAdmin)
