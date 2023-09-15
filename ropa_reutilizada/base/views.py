6f151f07d1dc84831518502de7924cd21eb94f4f
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-09-15 13:34:44 -0600

    quitar tablas

--- ropa_reutilizada/base/views.py
+++ ropa_reutilizada/base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
-
 # clase (herencia)
+
+def home(request):
+    return render(request, 'index.html')
+
+def login(request):
+      return render(request, 'InicioSesionCliente.html')
+
+def registro(request):
+      return render(request, 'RegistroCliente.html')
